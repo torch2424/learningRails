f9f3f603411e6e5e1c722d387ab81605da6db8ef
Aaron Turner <aaron@aaronthedev.com>
2018-01-08 20:35:52 -0800

    Started making some cool stuff

--- sleepyMirror/app/controllers/status_controller.rb
+++ sleepyMirror/app/controllers/status_controller.rb
@@ -1,7 +1,19 @@
 class StatusController < ApplicationController
   def test
-    render json: {
-      "yo": "it's lit"
-    }
+
+    # json in rails:
+    # https://stackoverflow.com/questions/3183786/how-to-convert-a-ruby-hash-object-to-json
+    response = {}
+
+    # Play with some params
+    # https://stackoverflow.com/questions/4352839/rails-how-to-get-all-parameters-from-url
+    response[:status] = request.path_parameters[:status]
+
+    request.query_parameters.each do |key,value|
+      Rails.logger.warn "Param #{key}: #{value}"
+      response[key] = value
+    end
+
+    render json: response
   end
 end
