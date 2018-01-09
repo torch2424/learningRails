33f4fb3ebd7cbdc9af402243c8ddc62277faaf7d
Aaron Turner <aaron@aaronthedev.com>
2018-01-08 20:50:36 -0800

    built an okay route

--- sleepyMirror/app/controllers/status_controller.rb
+++ sleepyMirror/app/controllers/status_controller.rb
@@ -1,5 +1,12 @@
 class StatusController < ApplicationController
-  def test
+
+  def index
+    render html: "Please pass a status code in the path"
+  end
+
+  def status
+
+
 
     # json in rails:
     # https://stackoverflow.com/questions/3183786/how-to-convert-a-ruby-hash-object-to-json
@@ -10,10 +17,13 @@ class StatusController < ApplicationController
     response[:status] = request.path_parameters[:status]
 
     request.query_parameters.each do |key,value|
-      Rails.logger.warn "Param #{key}: #{value}"
+      Rails.logger.debug "Param #{key}: #{value}"
       response[key] = value
     end
 
-    render json: response
+
+    # Colon Positioning
+    # https://stackoverflow.com/questions/28339025/ruby-on-rails-colon-at-back-or-front-of-variables
+    render json: response, status: request.path_parameters[:status]
   end
 end
--- sleepyMirror/config/routes.rb
+++ sleepyMirror/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get "status" => "status#test"
-  get "status/:status" => "status#test"
+  get "status" => "status#index"
+  get "status/:status" => "status#status"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
